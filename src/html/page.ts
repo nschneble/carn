9c6a3b867e177e49fd2cc2242ab0576fb822a7fd
Nick Schneble <nschneble@users.noreply.github.com>
2026-09-15 12:02:39 -0400

    add og:image companion metadata

--- src/html/page.ts
+++ src/html/page.ts
@@ -24,6 +24,10 @@ const head = (title: string, description: string, path: string) => html`<head>
 
     <meta property="og:description" content="${description}" />
     <meta property="og:image" content="${config.origin}/images/preview.jpg" />
+    <meta property="og:image:alt" content="A cairn of stacked stones, posterized in pink and lime" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="1200" />
     <meta property="og:site_name" content="Càrn" />
     <meta property="og:title" content="${title}" />
     <meta property="og:type" content="website" />
